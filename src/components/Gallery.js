66114a286e5448b8b7867e41e851529d9eb82429
Komoini <komolafeinioluwa9@gmail.com>
2023-05-27 12:44:53 +0100

    further commit

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,7 +8,15 @@ const Gallery = () => {
         <div>
             <Navbar/>
             <div>
-                <h1 className="">Recently sold out ARTs</h1>
+                <h1 className=" text-center py-[10px] text-[#395B64] font-heading2 font-semibold lg:text-[34px] lg:py-[19px] pl-1 text-[28px] leading-[19px] tracking-[2.7px]">Recently sold out ARTs</h1>
+                <section id='sold'>
+                    <div className=''>
+                    </div>
+                </section>
+                <section id='avail'>
+                    <div className=''>
+                    </div>
+                </section>
             </div>
             <Footer/>
         </div>
